82525b501f7af5f6cc37641502af9da2fb6c0afb
mbrown11222-sys <m.brown11222@gmail.com>
2026-04-07 14:56:46 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 5cdb919e66ba0a243cd8402bcb35972844af801f.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,3 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by mbrown11222-sys
- # Here is another update by mbrown11222-sys
